6c76c48002e5de38f411906f896e010df2e75d3f
Mohamed amine Ben Nourddine <mohamed.bennoureddine@metis-innovations.com>
2020-02-24 15:25:30 +0100

    Ajoutez des fichiers projet.

---
+++ DotNet-Core-Mvc-App-CI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNet_Core_Mvc_App_CI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
